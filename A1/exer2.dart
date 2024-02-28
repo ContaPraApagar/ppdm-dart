891d35ad53afc5e8c5e5eb1ce278ef699cc8b041
Nicolas Macedo <133895137+ContaPraApagar@users.noreply.github.com>
2024-02-28 08:11:41 -0300

    Add files via upload

---
+++ A1/exer2.dart
@@ -0,0 +1,9 @@
+void main() {
+   var num = 9;
+
+   //OPERADOR TERNARIO
+   var mensagem = num % 2 == 0 ? 'O número $num é par.' : 'O número $num é impar.';
+
+   //Imprimir a mensagem
+   print (mensagem);
+}
---
+++ A1/exer3.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  var num = 200;
+
+  //ESTRUTURA CONDICIONAL
+  if (num % 2 == 0) {
+    print('O número $num é par.');
+  }else {
+    print('O número $num é impar.');
+  }
+}
