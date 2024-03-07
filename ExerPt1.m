4729ffc9bcdd404e89a3fdbbc7eece40e212ce2d
Nicolas Macedo <133895137+ContaPraApagar@users.noreply.github.com>
2024-03-06 22:21:33 -0300

    Update ExerPt1.m

--- ExerPt1.m
+++ ExerPt1.m
@@ -1,5 +1,5 @@
 id main() {
-  List numeros = [10.0, 8.0, 9.0, 7.0, 10.0, 7.5, 9.0, 9.4];
+  List numeros = [10.0, 7.2, 9.0, 7.0, 8.5, 7.0, 10.0, 8.0];
 
   double soma = 0.0;
   for (double numero in numeros) {
