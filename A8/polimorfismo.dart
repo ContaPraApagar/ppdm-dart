50e5729d83087b9f8fc8f6fb49ce302dd5a87505
Nicolas Macedo <133895137+ContaPraApagar@users.noreply.github.com>
2024-04-01 12:15:37 -0300

    .

---
+++ A8/polimorfismo.dart
@@ -0,0 +1,27 @@
+import 'classes.dart';
+
+void main() {
+  Lobo lobo = new Lobo();
+  lobo.emitirsom();
+  lobo.dormir();
+  lobo.correr();
+  lobo.caminhar();
+
+  Cachorro cachorro = new Cachorro();
+  cachorro.emitirsom();
+  cachorro.dormir();
+  cachorro.correr();
+  cachorro.caminhar();
+
+  Leao leao = new Leao();
+  leao.emitirsom();
+  leao.dormir();
+  leao.correr();
+  leao.caminhar();
+
+  Gato gato = new Gato();
+  gato.emitirsom();
+  gato.dormir();
+  gato.correr();
+  gato.caminhar();
+}
